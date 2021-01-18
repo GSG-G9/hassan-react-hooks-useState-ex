93b5859d82dc5b8a03789393997131cc44c45c5f
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-01-18 14:37:52 +0200

    edit

--- src/components/Exercises2/index.js
+++ src/components/Exercises2/index.js
@@ -4,12 +4,15 @@ export default function Exercises2() {
 	const colors = ['red', 'blue'];
 	const [background, setBackground] = useState();
 	return (
-		<div className='exercises-container'>
+		<div
+			className='exercises-container'
+			style={{ backgroundColor: background }}
+		>
 			<Link className='button' to='/'>
 				Home
 			</Link>
 
-			<div style={{ backgroundColor: background }}>
+			<div>
 				<ul>
 					{colors.map((color, index) => (
 						<li key={index}>
